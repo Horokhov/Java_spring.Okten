792dce3e73068167ec7b03f7ec8792d505163372
MaxHorokhov <maxhorokhov@gmail.com>
2022-12-15 11:08:17 +0200

    Lesson1_spring

---
+++ src/main/java/ua/com/owu/lessonsspring/controllers/MainController.java
@@ -0,0 +1,30 @@
+package ua.com.owu.lessonsspring.controllers;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import ua.com.owu.lessonsspring.Models.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class MainController {
+
+    ArrayList<Customer> customerList=new ArrayList<>();
+
+    public MainController() {
+        customerList.add(new Customer(5,"Max"));
+        customerList.add(new Customer(6,"Oleg"));
+        customerList.add(new Customer(7,"Boris"));
+
+    }
+
+    @GetMapping("/getlist")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ArrayList<Customer> getCustomerList() {
+        return customerList;
+    }
+}
