47859fbea46b333780751cd376d12b7228a78758
MaxHorokhov <maxhorokhov@gmail.com>
2022-12-15 10:09:14 +0200

    Lesson1_spring

---
+++ src/main/java/ua/com/owu/lessonsspring/LessonsSpringApplication.java
@@ -0,0 +1,13 @@
+package ua.com.owu.lessonsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessonsSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/com/owu/lessonsspring/LessonsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.owu.lessonsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonsSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
