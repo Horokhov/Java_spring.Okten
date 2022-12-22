48b5db4e895a1c2553d6d935946a9ad3b522779a
MaxHorokhov <maxhorokhov@gmail.com>
2022-12-22 11:53:26 +0200

    Fix

--- src/main/java/ua/com/owu/lessonsspring/dao/CustomerDAO.java
+++ src/main/java/ua/com/owu/lessonsspring/dao/CustomerDAO.java
@@ -3,7 +3,7 @@ package ua.com.owu.lessonsspring.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import ua.com.owu.june2022springboot.models.Customer;
+import ua.com.owu.lessonsspring.models.Customer;
 
 import java.util.List;
 
